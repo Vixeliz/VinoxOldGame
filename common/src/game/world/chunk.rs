e893d34b1294c334e1a56c59c30d0ad5680f8af8
Mylah Dee <vixeliz@icloud.com>
2023-02-16 20:48:59 -0600

    Fix up palette system

--- common/src/game/world/chunk.rs
+++ common/src/game/world/chunk.rs
@@ -73,11 +73,14 @@ pub struct RawChunk {
 }
 
 impl RawChunk {
-    pub fn new() -> Option<RawChunk> {
-        Some(RawChunk {
+    // Very important to use this for creation because of air
+    pub fn new() -> RawChunk {
+        let mut raw_chunk = RawChunk {
             palette: Vec::new(),
             voxels: [Voxel((0, false)); TOTAL_CHUNK_SIZE as usize],
-        })
+        };
+        raw_chunk.palette.push("air".to_string());
+        raw_chunk
     }
 
     pub fn get_index_for_state(&self, block_data: &String) -> Option<usize> {
@@ -96,21 +99,55 @@ impl RawChunk {
         }
     }
 
+    // This is most likely a VERY awful way to handle this however for now I just want a working solution ill
+    // rewrite this if it causes major performance issues
+    pub fn update_chunk_pal(&mut self, old_vec: &Vec<String>) {
+        for i in 0..self.voxels.len() {
+            if let Some(block_data) = old_vec.get(self.voxels[i].0 .0 as usize) {
+                if let Some(new_index) = self.get_index_for_state(block_data) {
+                    self.voxels[i] = Voxel((new_index as u16, true)); // TODO: Transluency
+                } else {
+                    self.voxels[i] = Voxel((0, false));
+                }
+            }
+        }
+    }
+
     pub fn add_block_state(&mut self, block_data: &String) {
+        let old_vec = self.palette.clone();
         if let Some(id) = self.get_index_for_state(block_data) {
             warn!("Block data: {}, already exist!", block_data);
         } else {
             self.palette.push(block_data.to_owned());
+            self.update_chunk_pal(&old_vec);
         }
     }
     pub fn remove_block_state(&mut self, block_data: &String) {
+        if block_data.eq(&"air".to_string()) {
+            return;
+        }
+        let old_vec = self.palette.clone();
         if let Some(id) = self.get_index_for_state(block_data) {
             self.palette.remove(id);
+            self.update_chunk_pal(&old_vec);
         } else {
             warn!("Block data: {}, doesn't exist!", block_data);
         }
     }
-    pub fn set_voxel(&mut self, index: usize, block_data: String) {}
+    pub fn set_block(&mut self, index: usize, block_data: String) {
+        if let Some(block_type) = self.get_index_for_state(&block_data) {
+            self.voxels[index] = Voxel((block_type as u16, true)); // TODO: Set translucent based off of block
+        } else {
+            warn!("Voxel doesn't exist");
+        }
+    }
+    pub fn get_block(&mut self, index: usize) -> Option<String> {
+        if let Some(block_state) = self.get_state_for_index(self.voxels[index].0 .0 as usize) {
+            Some(block_state)
+        } else {
+            None
+        }
+    }
 }
 
 #[cfg(test)]
@@ -129,17 +166,15 @@ mod tests {
                 }
             }
         }
-        let mut raw_chunk = RawChunk {
-            palette: Vec::new(),
-            voxels,
-        };
+        let mut raw_chunk = RawChunk::new();
+        let i = ChunkShape::linearize([1, 1, 1]);
         raw_chunk.add_block_state(&"test".to_string());
+        raw_chunk.set_block(i as usize, "test".to_string());
         raw_chunk.add_block_state(&"test1".to_string());
         raw_chunk.remove_block_state(&"test".to_string());
-        println!(
-            "{:?}\n",
-            raw_chunk.get_index_for_state(&"test1".to_string())
-        );
+        println!("{:?}\n", raw_chunk.get_block(i as usize));
+        assert_eq!(raw_chunk.get_index_for_state(&"test1".to_string()), Some(1));
+        assert_eq!(raw_chunk.get_state_for_index(1), Some("test1".to_string()));
         println!("{:?}\n", raw_chunk.get_state_for_index(0));
     }
 }
