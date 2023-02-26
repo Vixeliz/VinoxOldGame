af7709a110ad5a7e22dbf213c4f61a890f95b57c
Mylah Dee <vixeliz@icloud.com>
2023-02-25 21:20:47 -0600

    Ridged Noise

--- server/src/game/world/generation.rs
+++ server/src/game/world/generation.rs
@@ -1,24 +1,22 @@
 use bevy::prelude::*;
 use common::game::world::chunk::*;
-use noise::*;
+use noise::{BasicMulti, HybridMulti, MultiFractal, NoiseFn, OpenSimplex, RidgedMulti};
 use rand::Rng;
 
 pub fn generate_chunk(pos: IVec3, seed: u32) -> RawChunk {
     //TODO: Switch to using ron files to determine biomes and what blocks they should use. For now hardcoding a simplex noise
-    let simplex_noise = OpenSimplex::new(seed);
-
+    let ridged_noise: RidgedMulti<OpenSimplex> =
+        RidgedMulti::new(seed).set_octaves(8).set_frequency(0.25);
     let mut raw_chunk = RawChunk::new();
-    for z in 1..(CHUNK_SIZE - 1) {
-        for y in 1..(CHUNK_SIZE - 1) {
-            for x in 1..(CHUNK_SIZE - 1) {
+    for x in 1..(CHUNK_SIZE - 1) {
+        for z in 1..(CHUNK_SIZE - 1) {
+            for y in 1..(CHUNK_SIZE - 1) {
                 let full_x = x as i32 + ((CHUNK_SIZE as i32) * pos.x);
                 let full_z = z as i32 + ((CHUNK_SIZE as i32) * pos.z);
                 let full_y = y as i32 + ((CHUNK_SIZE as i32) * pos.y);
-                let mut noise_val =
-                    simplex_noise.get([full_x as f64 / 100.0, full_z as f64 / 100.0]) * 50.0;
-                noise_val += simplex_noise.get([full_x as f64 / 50.0, full_z as f64 / 50.0]) * 25.0;
-                noise_val += simplex_noise.get([full_x as f64 / 25.0, full_z as f64 / 25.0]) * 12.5;
-                noise_val += simplex_noise.get([full_x as f64 / 12.5, full_z as f64 / 12.5]) * 6.25;
+                let noise_val =
+                    ridged_noise.get([(full_x as f64 / 100.0), (full_z as f64 / 100.0)]) * 100.0;
+
                 if full_y as f64 <= noise_val && full_y as f64 >= (noise_val - 2.0) {
                     raw_chunk.add_block_state(&"vinoxgrass".to_string());
                     raw_chunk.set_block(
@@ -36,11 +34,11 @@ pub fn generate_chunk(pos: IVec3, seed: u32) -> RawChunk {
                         .set_block(UVec3::new(x as u32, y as u32, z as u32), "air".to_string());
                 }
 
-                let mut multi_noise = simplex_noise.get([
-                    full_x as f64 / 10.0,
-                    full_z as f64 / 10.0,
-                    full_y as f64 / 10.0,
-                ]);
+                // let multi_noise = ridged_noise.get([
+                //     full_x as f64 / 10.0,
+                //     full_z as f64 / 10.0,
+                //     full_y as f64 / 10.0,
+                // ]);
 
                 // let change = if full_y >= -16 {
                 //     (full_y / 8) as f64
@@ -50,10 +48,10 @@ pub fn generate_chunk(pos: IVec3, seed: u32) -> RawChunk {
 
                 // multi_noise *= change;
 
-                if multi_noise >= 0.25 {
-                    raw_chunk
-                        .set_block(UVec3::new(x as u32, y as u32, z as u32), "air".to_string());
-                }
+                // if multi_noise >= 0.25 {
+                //     raw_chunk
+                //         .set_block(UVec3::new(x as u32, y as u32, z as u32), "air".to_string());
+                // }
             }
         }
     }
