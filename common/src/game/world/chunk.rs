d3e3095d44bb2ba93887abb32295c4c4a116c4f3
Mylah Dee <vixeliz@icloud.com>
2023-03-01 20:22:31 -0600

    Further

--- common/src/game/world/chunk.rs
+++ common/src/game/world/chunk.rs
@@ -197,9 +197,9 @@ pub fn world_to_voxel(voxel_pos: Vec3) -> (IVec3, UVec3) {
     (
         world_to_chunk(voxel_pos),
         UVec3::new(
-            voxel_pos.x.rem_euclid(CHUNK_SIZE as f32).floor() as u32,
-            voxel_pos.y.rem_euclid(CHUNK_SIZE as f32).floor() as u32,
-            voxel_pos.z.rem_euclid(CHUNK_SIZE as f32).floor() as u32,
+            voxel_pos.x.rem_euclid(CHUNK_SIZE as f32).floor() as u32 + 1,
+            voxel_pos.y.rem_euclid(CHUNK_SIZE as f32).floor() as u32 + 1,
+            voxel_pos.z.rem_euclid(CHUNK_SIZE as f32).floor() as u32 + 1,
         ),
     )
 }
